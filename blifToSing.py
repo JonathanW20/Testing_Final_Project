553ae606d59c0013de67cfaed8e46eb67e6c89e7
Jonathan Wang <jonathanw0619@gmail.com>
2023-12-16 20:42:37 -0700

    Updated python file

--- blifToSing.py
+++ blifToSing.py
@@ -1,58 +1,46 @@
-def convert_blif_to_sing(blif_content, sing_file):
-    # Initialize the Singular content with introductory remarks or comments
-    sing_content = '// Converted from BLIF to Singular\n'
+def convert_blif_to_singular(blif_content):
+    singular_code = ''
+    gate_map = {
+        'nand2': 'nand',
+        'inv1x': 'not',
+        'xor': 'xor'  # Adding XOR gate mapping
+    }
 
-    # Parsing BLIF content
     lines = blif_content.split('\n')
-    model_name = None
-    inputs = []
-    outputs = []
-    logic_gates = {}
-
     for line in lines:
         line = line.strip()
-        if line.startswith('.model'):
-            model_name = line.split()[-1]
-            sing_content += f'ring R = 0, ({model_name}), dp;\n'
-        elif line.startswith('.inputs'):
-            inputs = line.split()[1:]
-            for var in inputs:
-                sing_content += f'poly {var};\n'
-        elif line.startswith('.outputs'):
-            outputs = line.split()[1:]
-            for var in outputs:
-                sing_content += f'poly {var};\n'
-        elif line.startswith('.names'):
+        if line.startswith('.gate'):
             parts = line.split()
-            input_vars = parts[1:-1]
-            output_var = parts[-1]
-            truth_table = parts[2:-1]  # Exclude input/output variables
-            logic_gates[output_var] = {'input_vars': input_vars, 'truth_table': truth_table}
+            gate_type = parts[1]
+            gate_inputs = [part.split('=')[1] for part in parts[2:]]
 
-    # Generating Singular commands for logic gates
-    for output_var, gate_info in logic_gates.items():
-        sing_content += f"{output_var} = "
-        for i, tt in enumerate(gate_info['truth_table']):
-            if tt == '1':
-                sing_content += "(" + "*".join(gate_info['input_vars'][i] for i, bit in enumerate(tt) if bit == '1') + ")"
-                sing_content += "+"  # Adding "+" between minterms
-        sing_content = sing_content.rstrip("+")  # Remove the last "+"
-        sing_content += ";\n"
+            singular_gate = gate_map.get(gate_type)
+            if singular_gate:
+                if gate_type == 'inv1x':
+                    singular_code += f"{gate_inputs[0]} = {singular_gate}({gate_inputs[1]});\n"
+                else:
+                    # Include XOR gate handling
+                    if gate_type == 'xor2':
+                        singular_code += f"{gate_inputs[2]} = {singular_gate}({gate_inputs[0]}, {gate_inputs[1]});\n"
+                    else:
+                        singular_code += f"{gate_inputs[2]} = {singular_gate}({gate_inputs[0]}, {gate_inputs[1]});\n"
 
-    # Writing the converted content to the Singular file
-    with open(sing_file, 'w') as f:
-        f.write(sing_content)
+    return singular_code
 
-# BLIF content provided by the user
-blif_content = '''
-.model bcd_7seg
-.inputs a b c d
-.outputs seg0 seg1 seg2 seg3 seg4 seg5 seg6
-.names d c b a min0
-0000 1
-# Remaining BLIF content...
-.end
-'''
+def convert_multiplier_blif_to_singular(file_path):
+    with open(file_path, 'r') as file:
+        blif_content = file.read()
 
-# Replace 'output.sing' with your desired file path
-convert_blif_to_sing(blif_content, 'output.sing')
+    singular_code = convert_blif_to_singular(blif_content)
+
+    output_file_path = file_path.replace('.blif', '.sing')
+
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(singular_code)
+
+    print(f"Singular code written to {output_file_path}")
+
+# Example usage for a 4-bit multiplier BLIF file
+# Replace '4bit_multiplier.blif' with the path to your 8-bit or 32-bit multiplier BLIF file
+convert_multiplier_blif_to_singular('4bit_multiplier.blif')
+
